272ceae79fa1798050fa770d427e4db40344ef12
朝暾夕月 <10249585@qq.com>
2026-02-05 10:29:49 +0800

    修改引用库

--- header.php
+++ header.php
@@ -23,6 +23,7 @@ if (empty($sys_companies)) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>企业报销管理系统</title>
+    <link href="https://cdn.jsdelivr.net/npm/remixicon@3.5.0/fonts/remixicon.css" rel="stylesheet">
     <link rel="stylesheet" href="style.css?v=<?php echo time(); ?>">
     <script>
         const GLOBAL_COMPANIES = <?php echo json_encode($sys_companies); ?>;
